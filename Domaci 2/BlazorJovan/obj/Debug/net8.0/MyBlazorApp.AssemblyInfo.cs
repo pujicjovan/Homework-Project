c163ad784c4574f24d2d422c67d9ceab64d97544
pujicjovan <pujicjovan@yahoo.com>
2024-11-28 14:28:38 +0100

    asda

--- Domaci 2/BlazorJovan/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ Domaci 2/BlazorJovan/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3773b7f64ca6d1825a9af073f9c3458b606ac2c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f097f9d6d1115c9409cfdc481923db28586d014")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
